1b1fddffbedafc705f91adca982cc08e0a9f46f5
Giuseppe Burtini <giuseppe@burtini.ca>
2025-11-30 17:16:19 -0800

    add assets to static deployment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/rli-oas-presentation" : "";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath:
-    process.env.NODE_ENV === "production" ? "/rli-oas-presentation" : "",
+  basePath: basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
